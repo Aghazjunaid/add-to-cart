1ce13f081970c47ccbfa3e6aadf0bf906843623f
unknown <aghazjunaid96@gmail.com>
2022-03-28 13:35:56 +0530

    product listing

---
+++ productDetail.js
@@ -0,0 +1,5 @@
+let productId = document.querySelector("#product-detail")
+console.log(productId)
+console.log(window.location.href)
+const params = new URLSearchParams(window.location.search);
+console.log(params);
